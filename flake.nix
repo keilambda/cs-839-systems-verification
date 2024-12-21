33444eabe4786288e8fc8786260f47f1d5f0eb5f
thelissimus <thelissimus@tuta.io>
2024-12-21 13:20:50 +0500

    update(flake): coq version

--- flake.nix
+++ flake.nix
@@ -7,18 +7,18 @@
   outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
-      coqPackages = pkgs.coqPackages_8_20;
+      coqPackages = pkgs.coqPackages;
     in
     {
       devShells.default = pkgs.mkShell {
         buildInputs = with pkgs; [
-          coq_8_20
+          coq
           coqPackages.stdpp
           coqPackages.iris
         ];
 
         shellHook = ''
-          export COQPATH="${coqPackages.stdpp}/lib/coq/${pkgs.coq_8_20.coq-version}/user-contrib/:${coqPackages.iris}/lib/coq/${pkgs.coq_8_20.coq-version}/user-contrib/"
+          export COQPATH="${coqPackages.stdpp}/lib/coq/${pkgs.coq.coq-version}/user-contrib/:${coqPackages.iris}/lib/coq/${pkgs.coq.coq-version}/user-contrib/"
         '';
       };
     });
